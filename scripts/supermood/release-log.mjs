d6334f0f4c7eb1b8f62ec96f7c6013f6a8a22356
Florian Casset <floriancasset@gmail.com>
2023-06-12 09:05:24 +0200

    fix(releaselog): fix bad link

--- scripts/supermood/release-log.mjs
+++ scripts/supermood/release-log.mjs
@@ -137,7 +137,7 @@ const getFullLog = (formattedCommitsList, version) => `
 Here is the changelog for this new version:
 ${formattedCommitsList.join('\n')}
   1. :unlock: Login: https://preprod.supermood.co/auth/sso/saml20?sk=supermood-fr
-  2. :rocket: Test: https://v${version}-dot-preprod-supermood.appspot.com/auth/domain-switch/go
+  2. :rocket: Test: https://v${version.replace('.', '-')}-dot-preprod-supermood.appspot.com/auth/domain-switch/go
 
   Please react with :heavy_check_mark: or :x: after testing your own commits.
 `;
