05772dcb2b8e004436448b82d4df42ce08ef4e0b
Flotteur <floriancasset@gmail.com>
2025-01-14 10:57:18 +0100

    build(typescript): build files instead of executing `npx` (#7)

--- .source.sh
+++ .source.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env sh
 
 # git relative
-alias cleanLocalBranches="npx tsx $SHELL_ALIAS_DIR/scripts/clean-local-branches.ts"
-alias gitCleanTags="npx tsx $SHELL_ALIAS_DIR/scripts/git-tags-clean.ts"
-alias gswitch="npx tsx $SHELL_ALIAS_DIR/scripts/git-switch-branch.ts"
+alias cleanLocalBranches="node $SHELL_ALIAS_DIR/dist/clean-local-branches.js"
+alias gitCleanTags="node $SHELL_ALIAS_DIR/dist/git-tags-clean.js"
+alias gswitch="node $SHELL_ALIAS_DIR/dist/git-switch-branch.js"
 
 # personal
 alias gs="git status"
 alias gskip="git rebase --skip"
 
 # supermood specific
-alias generateRelease="npx tsx $SHELL_ALIAS_DIR/scripts/supermood/create-release.ts"
-alias releaseLog="npx tsx $SHELL_ALIAS_DIR/scripts/supermood/release-log.ts"
+alias generateRelease="node $SHELL_ALIAS_DIR/dist/supermood/create-release.js"
+alias releaseLog="node $SHELL_ALIAS_DIR/dist/supermood/release-log.js"
