e35eed4d9025b5cd8c431b32ab8fd6d5bb127c46
$omeone <floriancasset@gmail.com>
2025-04-04 10:01:17 +0200

    feat(git-switch-branch): prompt a branch choice when multiple occurences found (#11)

--- scripts/git-switch-branch.ts
+++ scripts/git-switch-branch.ts
@@ -1,3 +1,4 @@
+import prompts from 'prompts';
 import {getRemoteBranchesList, switchLocalBranch} from './helpers/git';
 import {colorize, colorKeys} from './helpers/shell-colors';
 
@@ -10,22 +11,41 @@ if (!branchToFind) {
 
 const foundBranches = getRemoteBranchesList()
   .map(line => line.trim().replace(/\s.*/, ''))
-  .filter(data => !!data)
-  .filter(branchName => branchName.includes(branchToFind))
+  .filter(branchName => !!branchName && branchName.includes(branchToFind))
   .map(branchName => branchName.replace(/origin\//, ''));
 
-if (!foundBranches.length) {
-  console.log(colorize(`⚠️ No branch found with given name "${branchToFind}"`, colorKeys.yellow));
-}
-else if (foundBranches.length === 1) {
-  const [branchToSwitch] = foundBranches;
+const getBranchToSwitch = async (branchesList: string[]) => {
+  switch (branchesList.length) {
+    case 0: {
+      console.log(colorize(`⚠️ No branch found with given name "${branchToFind}"`, colorKeys.yellow));
 
+      return '';
+    }
+    case 1: {
+      const [switchingBranch] = branchesList;
+
+      return switchingBranch;
+    }
+    default: {
+      console.log(colorize(`⚠️ ${branchesList.length} branches found with given name`, colorKeys.yellow));
+
+      const {branchToSwitch: switchingBranch} = await prompts({
+        choices: branchesList.map(branchName => ({title: branchName, value: branchName})),
+        message: 'Which branch to switch on?',
+        name: 'branchToSwitch',
+        type: 'select',
+      }) as {branchToSwitch: string};
+
+      return switchingBranch;
+    }
+  }
+};
+
+const branchToSwitch = await getBranchToSwitch(foundBranches);
+
+if (branchToSwitch) {
   switchLocalBranch(branchToSwitch);
-}
-else {
-  // TODO ask user to choose branch with prompt autocomplete option
-  console.log(colorize(`⚠️ Multiple branches found with given name "${branchToFind}":\n${foundBranches.join('\n ')}`, colorKeys.yellow));
+  console.log(colorize('✅ Finished', colorKeys.green));
 }
 
-console.log(colorize('✅ Finished', colorKeys.green));
 process.exit(0);
